5d88fa66eb351ce9806771e5c7b0aa724b91e212
Zabrane <zabrane3@gmail.com>
2024-08-30 16:13:41 +0200

    Update inotifywait.erl

--- src/sys/inotifywait.erl
+++ src/sys/inotifywait.erl
@@ -11,7 +11,7 @@ start_port(Path, Cwd) ->
     %%        "-m", "-e", "modify", "-e", "close_write", "-e", "moved_to", "-e", "moved_from", "-e", "create", "-e", "delete",
     %%        "-e", "attrib", "--quiet", "-r", Path1],
     Args = ["-c", "inotifywait \"$0\" \"$@\" & PID=$!; read a; kill $PID",
-            "-m", "-e", "close_write", "--quiet", Path1],
+            "-m", "-e", "create", "-e", "moved_to", "--quiet", Path1],
     erlang:open_port({spawn_executable, os:find_executable("sh")},
         [stream, exit_status, binary, {line, 1000000}, {args, Args}, {cd, Cwd}]).
 
