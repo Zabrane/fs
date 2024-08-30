fbe66dedcb0fe56c6e52fe680500a310602a2ec3
Zabrane <zabrane3@gmail.com>
2024-08-30 22:09:26 +0200

    Update inotifywait.erl

--- src/sys/inotifywait.erl
+++ src/sys/inotifywait.erl
@@ -5,40 +5,55 @@
 find_executable() -> os:find_executable("inotifywait").
 known_events() -> [created, deleted, renamed, closed, modified, isdir, attribute, undefined].
 
+%% compiled regular expression
+%% > rp(re:compile("^(.+) ([A-Z_,]+) (.+)$", [])).
+-define(RE_EVENT, {re_pattern,3,0,0,
+                <<69,82,67,80,139,0,0,0,16,0,0,0,65,0,0,0,255,255,255,
+                  255,255,255,255,255,0,0,32,0,0,0,3,0,0,0,64,0,0,0,0,0,
+                  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,131,0,
+                  71,27,133,0,7,0,1,87,12,120,0,7,29,32,133,0,39,0,2,
+                  110,0,0,0,0,0,16,0,0,254,255,255,135,0,0,0,0,0,0,0,0,
+                  0,0,0,0,0,0,0,0,0,0,0,0,107,120,0,39,29,32,133,0,7,0,
+                  3,87,12,120,0,7,25,120,0,71,0>>}).
+
+join([EventName|R]) ->
+join(R, ["-e",EventName]).
+
+join([EventName|R], Acc) ->
+join(R, ["-e", EventName | Acc]);
+join([], Acc) ->
+Acc.
+
 start_port(Path, Cwd) ->
     Path1 = filename:absname(Path),
-    %%Args = ["-c", "inotifywait \"$0\" \"$@\" & PID=$!; read a; kill $PID",
-    %%        "-m", "-e", "modify", "-e", "close_write", "-e", "moved_to", "-e", "moved_from", "-e", "create", "-e", "delete",
-    %%        "-e", "attrib", "--quiet", "-r", Path1],
+    Events = [ "modify", "close_write", "moved_to", "moved_from", "create", "delete", "attrib" ],
     Args = ["-c", "inotifywait \"$0\" \"$@\" & PID=$!; read a; kill $PID",
-            "-m", "-e", "create", "-e", "moved_to", "--quiet", Path1],
+            "-m" ] ++
+            join(Events) ++
+            [ "--quiet", Path1],
     erlang:open_port({spawn_executable, os:find_executable("sh")},
-        [stream, exit_status, binary, {line, 1000000}, {args, Args}, {cd, Cwd}]).
+        [stream, exit_status, binary, use_stdio, {args, Args}, {cd, Cwd}]).
 
-line_to_event(Line0) ->
-    Line = unicode:characters_to_list(Line0, utf8),   
-    {match, [Dir, Flags1, DirEntry]} = re:run(Line, re(), [{capture, all_but_first, list}]),
-    Flags = [convert_flag(F) || F <- string:tokens(Flags1, ",")],
-    Path = Dir ++ DirEntry,
-    {Path, Flags}.
+line_to_event(<<Data/binary>>) ->
+    Lines = lists:usort(binary:split(binary:replace(Data, <<"\r\n">>, <<"\n">>, [global]), <<"\n">>, [global])),
+    Fun = fun(<<>>, Acc) -> Acc;
+             (Line, Acc) -> case re:run(Line, ?RE_EVENT, [{capture, all_but_first, binary}]) of
+                                {match, [Dir, Flags, Entry]} -> [{<<Dir/binary, Entry/binary>>, flags(Flags)} | Acc];
+                                nomatch -> Acc
+                            end
+        end,
+    lists:foldl(Fun, [], Lines).
 
-convert_flag("CREATE") -> created;
-convert_flag("DELETE") -> deleted;
-convert_flag("ISDIR") -> isdir;
-convert_flag("MODIFY") -> modified;
-convert_flag("CLOSE_WRITE") -> modified;
-convert_flag("CLOSE") -> closed;
-convert_flag("MOVED_TO") -> renamed;
-convert_flag("MOVED_FROM") -> removed;
-convert_flag("ATTRIB") -> attribute;
-convert_flag(_) -> undefined.
+flags(Flags) ->
+   [ convert_flag(Flag) || Flag <- binary:split(Flags, <<",">>, [global]) ].
 
-re() ->
-    case get(inotifywait_re) of
-        undefined ->
-            {ok, R} = re:compile("^(.*/) ([A-Z_,]+) (.*)$", [unicode]),
-            put(inotifywait_re, R),
-            R;
-        V -> V
-    end.
-
+convert_flag(<<"CREATE">>)      -> created;
+convert_flag(<<"DELETE">>)      -> deleted;
+convert_flag(<<"ISDIR">>)       -> isdir;
+convert_flag(<<"MODIFY">>)      -> modified;
+convert_flag(<<"CLOSE_WRITE">>) -> modified;
+convert_flag(<<"CLOSE">>)       -> closed;
+convert_flag(<<"MOVED_TO">>)    -> renamed;
+convert_flag(<<"MOVED_FROM">>)  -> removed;
+convert_flag(<<"ATTRIB">>)      -> attribute;
+convert_flag(_)                 -> undefined.
